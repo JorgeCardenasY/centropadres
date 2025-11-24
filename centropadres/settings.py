7438f127b089258b807c09471e273109bf1f78fc
JorgeCardenasY <jorge.cardenas.y@gmail.com>
2025-11-24 07:18:54 -0300

    2Rendr.

--- centropadres/settings.py
+++ centropadres/settings.py
@@ -134,3 +134,15 @@ STORAGES = {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
+
+# Security Headers (apply only when DEBUG is False)
+if not DEBUG:
+    SECURE_SSL_REDIRECT = True
+    SECURE_HSTS_SECONDS = 31536000  # 1 year
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_BROWSER_XSS_FILTER = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    X_FRAME_OPTIONS = 'DENY' # Or 'SAMEORIGIN' if you need iframes from the same domain
